5dd6276e84be85b042bb8ccf765191c5a28aa479
booldook <booldook@gmail.com>
2018-12-28 12:46:50 +0900

    booldook

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,23 +1,8 @@
 //02efdd64bdc14b279bc91d9247db4722
 //api.openweathermap.org/data/2.5/weather?id=2172797
+//api.openweathermap.org/data/2.5/forecast?id=524901
 var log = console.log;
-$.ajax({
-	url: "https://api.openweathermap.org/data/2.5/weather",
-	type: "get",
-	dataType: "json",
-	data: {
-		id: "1835848",
-		appid: "02efdd64bdc14b279bc91d9247db4722",
-		units: "metric"
-	},
-	success: function(data){
-		log(data);
-	},
-	error: function(xhr){
-		log(xhr);
-	}
-});
-
+var urlBase = "https://api.openweathermap.org/data/2.5/";
 $(".tabs > li").click(function(){
 	var n = $(this).index();
 	$(".tabs > li").css({
@@ -33,6 +18,39 @@ $(".tabs > li").click(function(){
 		"font-weight":"bold"
 	});
 	$(".conts").hide();
-	$(".conts").eq(n).show();
+	$(".conts").eq(n).show(function(){
+		if(n == 0) url = urlBase + "weather";
+		else url = urlBase + "forecast";
+		$.ajax({
+			url: url,
+			type: "get",
+			dataType: "json",
+			data: {
+				id: "1835848",
+				appid: "02efdd64bdc14b279bc91d9247db4722",
+				units: "metric"
+			},
+			success: function(data){
+				if(n == 0) dailyFn(data);
+				else weeklyFn(data);
+			},
+			error: function(xhr){
+				log(xhr);
+			}
+		});
+	});
 });
 $(".tabs > li").eq(0).trigger("click");
+
+function dailyFn(data) {
+	console.log(data);
+	$("#wt_icon").attr("src", "../img/icon/"+data.weather[0].icon+".png");
+	$("#wt_main").html(data.weather[0].main+" / "+data.weather[0].description);
+	$("#wt_temp").html(data.main.temp+"("+data.main.temp_max+"/"+data.main.temp_min+")");
+	$("#wt_wind").html(data.wind.speed+"/ms ("+data.wind.deg+"deg)");
+	$("#wt_coord").html("위도: "+data.coord.lat+" / 경도: "+data.coord.lon);
+}
+
+function weeklyFn(data) {
+	log("weekly"+data);
+}
