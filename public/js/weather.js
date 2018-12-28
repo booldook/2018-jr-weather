67e2e0f2562d1bf81031f77abfa233002eef32ce
booldook <booldook@gmail.com>
2018-12-28 11:32:08 +0900

    ...

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,27 +1,19 @@
+//02efdd64bdc14b279bc91d9247db4722
+//api.openweathermap.org/data/2.5/weather?id=2172797
+var log = console.log;
 $.ajax({
-	type: "get",
 	url: "https://api.openweathermap.org/data/2.5/weather",
+	type: "get",
 	dataType: "json",
 	data: {
-		id: "1846266",
+		id: "1835848",
 		appid: "02efdd64bdc14b279bc91d9247db4722",
 		units: "metric"
 	},
-	success: function(data) {
-		console.log(data);
-		var html = `
-		<div class="w3-padding">
-			<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
-		</div>
-		<div class="w3-padding">현재온도:${data.main.temp}도</div>
-		<div class="w3-padding">
-		현재날씨:${data.weather[0].main} / ${data.weather[0].description}</div>
-		<div class="w3-padding">현재바람:${data.wind.speed} / ${data.wind.deg}deg</div>
-		<div class="w3-padding">현재도시:${data.name}</div>
-		`;
-		$("#status").append(html);
+	success: function(data){
+		log(data);
 	},
-	error: function(xhr) {
-		console.log(xhr);
+	error: function(xhr){
+		log(xhr);
 	}
 });
